a2088fd59ce0c41c4d2f88e312636c5805d9eccd
john <john.koh@live.com>
2023-06-13 10:11:09 +0800

    added files and codes

---
+++ open_kattis/addtwonumbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class addtwonumbers{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int option1 = scanner.nextInt();
+        int option2 = scanner.nextInt();
+        int total = option1 + option2;
+        
+        System.out.println(total);
+        
+    }
+}
