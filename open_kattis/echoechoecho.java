f6ad32129c997128b3b178df13e53940e0ed0573
john <john.koh@live.com>
2023-06-28 15:26:35 +0800

    new challenge

---
+++ open_kattis/echoechoecho.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class echoechoecho{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String  input= scanner.nextLine();
+        
+        for(int i=0; i<3; i++){
+        System.out.print(input + " ");            
+        }
+    }
+}
