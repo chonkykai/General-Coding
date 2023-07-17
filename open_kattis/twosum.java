aae8ae2580a8e9dba76b26adfde50d4fe27c8394
john <john.koh@live.com>
2023-07-17 08:44:02 +0800

    new challenge

---
+++ open_kattis/twosum.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class twosum{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int input1 = scanner.nextInt();
+        int input2 = scanner.nextInt();
+        
+        System.out.println(input1 + input2);
+        
+    }
+}
